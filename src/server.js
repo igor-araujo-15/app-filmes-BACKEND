2f5738a4b938493903367d13b71a5dfd130e0b21
Igor Alves <igoorzinh@gmail.com>
2026-06-13 11:14:33 -0300

    Ajusta porta para Render

--- src/server.js
+++ src/server.js
@@ -11,14 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', filmeRoutes);
+
+app.get('/', (req, res) => {
+  res.json({ mensagem: 'API de Filmes funcionando.' });
+});
+
 app.get('/teste', (req, res) => {
   res.send('API OK');
 });
+
+const PORT = process.env.PORT || 3000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(3000, '0.0.0.0', () => {
-      console.log('Servidor rodando na porta 3000');
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
   })
   .catch((erro) => {
